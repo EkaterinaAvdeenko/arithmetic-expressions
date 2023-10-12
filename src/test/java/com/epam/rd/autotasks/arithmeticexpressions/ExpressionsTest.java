ca4d3b9abc04d1876ae897ad6e4a411497ab4976
Ekaterina Avdeenko <avdeenqoeka@gmail.com>
2023-10-12 16:28:02 +0000

    Update ExpressionsTest.java

--- src/test/java/com/epam/rd/autotasks/arithmeticexpressions/ExpressionsTest.java
+++ src/test/java/com/epam/rd/autotasks/arithmeticexpressions/ExpressionsTest.java
@@ -1,6 +1,5 @@
 package com.epam.rd.autotasks.arithmeticexpressions;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
